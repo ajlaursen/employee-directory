a60a684a2815ec513333b82d96415e8c5b70325c
Alex Laursen <ajlaursen@aol.com>
2021-02-06 16:00:16 -0500

    MVP

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -5,8 +5,11 @@ import React, { useState, useEffect } from "react";
 
 
 function App() {
+  // all the info
   const [userArray, setUserArray] = useState([]);
+  // nationalities in list
   const [natArray, setNatArray] = useState([]);
+  const [filteredArray, setFilteredArray] =useState([])
 
   useEffect(() => {
     axios
@@ -17,34 +20,52 @@ function App() {
           a.name.first.localeCompare(b.name.first)
         );
         setUserArray(sortedArray);
+        setFilteredArray(sortedArray);
 
         // i feel like there should be an easier way to do the below process
 
-        // creating an array with only nationalities
-        const transferArray = []
-        sortedArray.forEach(function(element, index) {
-          transferArray.push(element.nat)
-        });
-
-        // filtering duplicates out of list
-        const toBeKeyedArray = transferArray.filter(function(item, pos) {
-          return transferArray.indexOf(item) === pos;
-        })
+        const natsArray = Array.from(new Set(sortedArray.map(x => x.nat))).sort();
         
-        // adding key to list for react purposes 
-        const listArray = []
-        toBeKeyedArray.forEach(function(element, index) {
-          listArray.push({"key": index +1, "nat": element})
-        })
+        // creating an array with only nationalities
+        // const transferArray = []
+        // sortedArray.forEach(function(element, index) {
+        //   transferArray.push(element.nat)
+        // });
 
-        console.log("sortedArray",sortedArray)
-      console.log("natArray",listArray)
-        setNatArray(listArray)
+        // // filtering duplicates out of list
+        // const toBeKeyedArray = transferArray.filter(function(item, pos) {
+        //   return transferArray.indexOf(item) === pos;
+        // })
+        
+        // // adding key to list for react purposes 
+        // const listArray = []
+        // toBeKeyedArray.forEach(function(element, index) {
+        //   listArray.push({"key": index +1, "nat": element})
+        // })
+
+       
+  
+        setNatArray(natsArray)
       })
       .catch((err) => console.log(err));
   },[]);
 
- 
+ function handleListChange(e){
+   switch(e.target.value){
+   case "Select county":
+    break;
+    default:
+    const employeeFilteredArray = userArray.filter(element => {
+      return e.target.value === element.nat
+    })
+    setFilteredArray(employeeFilteredArray)
+  }
+    console.log("we changed", e.target.value)
+     
+
+  // take the array and then filter from result then reset user array
+
+ }
 // need help being able to use the drop down menu you filter by country
 
 
@@ -53,11 +74,11 @@ function App() {
       <header>
         <h1>Employee Directory</h1>
       </header>
-      <select className="form-select" aria-label="Default select example">
+      <select className="form-select" aria-label="Default select example" onChange={handleListChange}>
         <option defaultValue>Select Country</option>
-        {natArray.map((result) => (
-          <option key={result.key} value={result.nat}>
-            {result.nat}
+        {natArray.map((result, index) => (
+          <option  key={index} value={result}>
+            {result}
           </option>
         ))}
       </select>
@@ -68,14 +89,16 @@ function App() {
               <th scope="col">First</th>
               <th scope="col">last</th>
               <th scope="col">Email</th>
+              <th scope="col"> Nationality</th>
             </tr>
           </thead>
           <tbody>
-            {userArray.map((result) => (
+            {filteredArray.map((result) => (
               <tr key={result.login.uuid}>
                 <td>{result.name.first}</td>
                 <td>{result.name.last}</td>
                 <td>{result.email}</td>
+                <td>{result.nat}</td>
               </tr>
             ))}
           </tbody>
