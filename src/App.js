4d3dfc33a64d732b7c4acc313a7315f10f9a3501
Alex Laursen <ajlaursen@aol.com>
2021-02-09 15:55:41 -0500

    fixed select country returning to all users

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import axios from "axios";
 import "./App.css";
 import React, { useState, useEffect } from "react";
 
-
-
 function App() {
   // all the info
   const [userArray, setUserArray] = useState([]);
   // nationalities in list
   const [natArray, setNatArray] = useState([]);
-  const [filteredArray, setFilteredArray] =useState([])
+  const [filteredArray, setFilteredArray] = useState([]);
 
   useEffect(() => {
     axios
@@ -24,8 +22,10 @@ function App() {
 
         // i feel like there should be an easier way to do the below process
 
-        const natsArray = Array.from(new Set(sortedArray.map(x => x.nat))).sort();
-        
+        const natsArray = Array.from(
+          new Set(sortedArray.map((x) => x.nat))
+        ).sort();
+
         // creating an array with only nationalities
         // const transferArray = []
         // sortedArray.forEach(function(element, index) {
@@ -36,48 +36,49 @@ function App() {
         // const toBeKeyedArray = transferArray.filter(function(item, pos) {
         //   return transferArray.indexOf(item) === pos;
         // })
-        
-        // // adding key to list for react purposes 
+
+        // // adding key to list for react purposes
         // const listArray = []
         // toBeKeyedArray.forEach(function(element, index) {
         //   listArray.push({"key": index +1, "nat": element})
         // })
 
-       
-  
-        setNatArray(natsArray)
+        setNatArray(natsArray);
       })
       .catch((err) => console.log(err));
-  },[]);
+  }, []);
 
- function handleListChange(e){
-   switch(e.target.value){
-   case "Select county":
-    break;
-    default:
-    const employeeFilteredArray = userArray.filter(element => {
-      return e.target.value === element.nat
-    })
-    setFilteredArray(employeeFilteredArray)
+  function handleListChange(e) {
+    switch (e.target.value) {
+      case "Select Country":
+        // need to return to full user array
+        setFilteredArray(userArray);
+        break;
+      default:
+        const employeeFilteredArray = userArray.filter((element) => {
+          return e.target.value === element.nat;
+        });
+        setFilteredArray(employeeFilteredArray);
+    }
+    console.log("we changed", e.target.value);
+
+    // take the array and then filter from result then reset user array
   }
-    console.log("we changed", e.target.value)
-     
-
-  // take the array and then filter from result then reset user array
-
- }
-// need help being able to use the drop down menu you filter by country
-
+  // need help being able to use the drop down menu you filter by country
 
   return (
     <div className="App">
       <header>
         <h1>Employee Directory</h1>
       </header>
-      <select className="form-select" aria-label="Default select example" onChange={handleListChange}>
+      <select
+        className="form-select"
+        aria-label="Default select example"
+        onChange={handleListChange}
+      >
         <option defaultValue>Select Country</option>
         {natArray.map((result, index) => (
-          <option  key={index} value={result}>
+          <option key={index} value={result}>
             {result}
           </option>
         ))}
